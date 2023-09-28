2067634e23deebfd6d4acfc960dc49d584025989
Naveen Chinnadurai <111662328+NaveenChinnadurai@users.noreply.github.com>
2023-09-28 09:54:48 +0530

    added:switch b/t age calc & day finder

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import './App.css'
+import Calander from './components/calander'
 import DayFinder from './components/dayFinder'
 
 function App() {
@@ -7,12 +9,33 @@ function App() {
   const currentMonth = currDay.getMonth()
   const currentYear = currDay.getFullYear()
 
+  function DayFinderCompo() {
+    return (
+      <div className="dayfinder">
+        <div className="dayfinder-header">
+          <h2>Day Finder</h2>
+          <button onClick={() =>  setArea(<CalenderComp/>)} className='change-btn'>Age Calc</button>
+        </div>
+        <DayFinder yr={currentYear} />
+      </div>
+    )
+  }
+  function CalenderComp() {
+    return (
+      <div className="dayfinder">
+        <div className="dayfinder-header">
+          <h2>Age Calculator</h2>
+          <button onClick={() =>  setArea(<DayFinderCompo/>) } className='change-btn'>Day Finder</button>
+        </div>
+        <Calander currDate={currentDate} currMon={currentMonth} currYr={currentYear} />
+      </div>
+    )
+  }
+  const [area, setArea] = useState(<DayFinderCompo />)
   return (
     <div className="app">
-      <h2>Day Finder</h2>
-      <DayFinder yr={currentYear} />
+      {area}
     </div>
   )
 }
-
 export default App
